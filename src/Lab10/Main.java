3ce9391a3e966191670df050d019dfa90ab211cf
Andrei Tatov <andreitatov212@gmail.com>
2020-07-03 17:43:06 +0300

    finished lab 8 and 9

---
+++ src/Lab10/Main.java
@@ -0,0 +1,9 @@
+package Lab10;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        
+    }
+}
---
+++ src/Lab8/rabbit/Main.java
@@ -0,0 +1,15 @@
+package Lab8.rabbit;
+
+public class Main {
+    public static void main(String[] args) {
+        for(int i=0;i<10;i++){
+            if(i%2==0){
+                Thread th = new RabbitThread(i);
+                th.start();
+            }else{
+                Thread th = new Thread(new RabbitRunnable(i));
+                th.run();
+            }
+        }
+    }
+}
